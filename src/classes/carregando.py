b366b78783ea20219da636cafd7461f22b192143
Lyarkh <lcemanuel.emanuel@gmail.com>
2022-02-25 20:28:46 -0300

    melhorando classe carregamento

--- src/classes/carregando.py
+++ src/classes/carregando.py
@@ -1,13 +1,18 @@
+import os
 from time import sleep
 
 class Carregamento:
 
     @staticmethod
     def com_ponto():
+        os.system('cls')
+        
         print('Carregando', end='')
 
         ponto = '.'
         for _ in range(4):
             print(ponto, end='')
             sleep(0.2)
-            
+        
+        os.system('cls')
+        
--- src/classes/opcoes.py
+++ src/classes/opcoes.py
@@ -1,11 +1,13 @@
 from .classes_mae.template_opcao import TemplateOpcao
 from .classes_mae.voltar_menu import VoltarMenu
+from .carregando import Carregamento
 
 class Opcao01(TemplateOpcao, VoltarMenu):
 
     def run(self):
         print('esta funcionando a opção 01')
         self.volta_para_o_menu()
+        Carregamento.com_ponto()
 
     def muda_estado(self):
         return 1
@@ -15,6 +17,7 @@ class Opcao02(TemplateOpcao,VoltarMenu):
     def run(self):
         print('esta funcionando a opção 02')
         self.volta_para_o_menu()
+        Carregamento.com_ponto()
     
     def muda_estado(self):
         return 1
@@ -24,6 +27,7 @@ class Opcao03(TemplateOpcao,VoltarMenu):
     def run(self):
         print('esta funcionando a opção 03')
         self.volta_para_o_menu()
+        Carregamento.com_ponto()
     
     def muda_estado(self):
         return 1
@@ -32,12 +36,12 @@ class Encerramento(TemplateOpcao):
 
     def run(self):
         print('esta funcionando a opção 07')
-        input('pressione qualquer tecla para continuar')
+        Carregamento.com_ponto()
     
     def muda_estado(self):
         #utilizado para mudar o estado do menu e encerrar o programa.
-        print('-----------------------')
-        print('Encerrando o programa!!')
-        print(' ---até a próxima---!  ')
+        print('\n-----------------------')
+        print('Encerrando o programa!!  ')
+        print(' ---até a próxima---!    ')
 
         return 0  
